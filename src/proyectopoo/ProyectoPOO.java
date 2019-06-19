9977e2b88e8a3f29d72585e51bf24c4a4964df24
Frognas <rodash20@hotmail.com>
2019-06-19 16:23:19 -0600

    Linea de Rodash prueba 2

--- src/proyectopoo/ProyectoPOO.java
+++ src/proyectopoo/ProyectoPOO.java
@@ -17,6 +17,7 @@ public class ProyectoPOO {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Linea de Rodash");
+        System.out.println("AHHHH prueba2");
     }
     
 }
